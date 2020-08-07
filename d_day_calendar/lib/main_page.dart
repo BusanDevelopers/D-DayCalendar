e2432b883a94b8a266459e85627867bc26ca7ee6
Coalery <doralife12@naver.com>
2020-08-07 18:28:18 +0900

    Add: ListView under a dday indicator

--- d_day_calendar/lib/main_page.dart
+++ d_day_calendar/lib/main_page.dart
@@ -10,40 +10,48 @@ class MainPage extends StatelessWidget {
 
     return Scaffold(
       body: SafeArea(
-        child: ListView(
+        child: Stack(
           children: [
-            Stack(
+            ListView.builder(
+              padding: EdgeInsets.only(top: 220),
+              itemCount: 10,
+              itemBuilder: (_, index) {
+                return Container(
+                  width: double.infinity,
+                  height: 100,
+                  margin: EdgeInsets.all(20),
+                  color: Colors.grey,
+                );
+              },
+            ),
+            Container(
+              height: 220,
+              decoration: BoxDecoration(
+                color: Colors.lightBlue[300],
+                borderRadius: BorderRadius.only(bottomLeft: Radius.elliptical(width, 75), bottomRight: Radius.elliptical(width, 75))
+              )
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
-                  height: 220,
-                  decoration: BoxDecoration(
-                    color: Colors.lightBlue[300],
-                    borderRadius: BorderRadius.only(bottomLeft: Radius.elliptical(width, 75), bottomRight: Radius.elliptical(width, 75))
+                  width: double.infinity,
+                  height: 202,
+                  child: Swiper(
+                    autoplay: true,
+                    autoplayDelay: 5000,
+                    viewportFraction: 0.8,
+                    scale: 0.85,
+                    itemCount: 5,
+                    itemBuilder: (_, index) {
+                      return DdayUnit(dday: new Dday(new DateTime(2020, 8, 8), "서버 스터디 모임", "테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트"));
+                    },
                   )
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      width: double.infinity,
-                      height: 202,
-                      child: Swiper(
-                        autoplay: true,
-                        autoplayDelay: 5000,
-                        viewportFraction: 0.8,
-                        scale: 0.85,
-                        itemCount: 5,
-                        itemBuilder: (_, index) {
-                          return DdayUnit(dday: new Dday(new DateTime(2020, 8, 8), "서버 스터디 모임", "테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트테스트"));
-                        },
-                      )
-                    )
-                  ],
                 )
               ],
             )
           ],
-        ),
+        )
       ),
     );
   }
