c4ca9fb30e0287c60b210f5508028393f32d522a
tobe2111 <64677275+tobe2111@users.noreply.github.com>
2020-08-08 17:54:00 +0900

    Add: Create dday page

--- d_day_calendar/lib/main.dart
+++ d_day_calendar/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:d_day_calendar/create_dday_page.dart';
 import 'package:d_day_calendar/main_page.dart';
 import 'package:flutter/material.dart';
 
@@ -11,7 +12,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       routes: {
-        '/' : (context) => MainPage()
+        '/': (context) => MainPage(),
+        CreateDdayPage.routeName: (context) => CreateDdayPage()
       },
     );
   }
